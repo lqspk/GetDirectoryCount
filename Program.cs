d464ee56be2f5c73252735649ec733a35dead838
lqspk <66058194+lqspk@users.noreply.github.com>
2020-05-29 01:00:52 +0800

    增加同步非递归方式

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace GetDirectoryCount
@@ -45,20 +46,21 @@ namespace GetDirectoryCount
                 return;
             }
 
+            //当前输入的目录信息
+            DirectoryInfo root = new DirectoryInfo(path);
+
+            #region 多线程递归方式
             //重置计时
             stopwatch.Reset();
 
             //开始计时
             stopwatch.Start();
 
-            //当前输入的目录信息
-            DirectoryInfo root = new DirectoryInfo(path);
-
             //任务列表
             List<Task> taskList = new List<Task>();
 
             //获取当前输入的目录的文件数量
-            var task = Task.Run(() => { IncEmptyDirectoryCount(GetFileCount(root)); });
+            var task = Task.Run(() => { IncFileCount(GetFileCount(root)); });
             taskList.Add(task);
 
             //获取子目录
@@ -84,28 +86,51 @@ namespace GetDirectoryCount
             //时间转换
             DateTime time = ConvertToDateTime(timeSpan);
 
+            Console.WriteLine($"递归方式");
             Console.WriteLine($"运行框架：{RuntimeInformation.FrameworkDescription}");
             Console.WriteLine($"操作系统：{RuntimeInformation.OSDescription}");
             Console.WriteLine($"平台架构：{RuntimeInformation.OSArchitecture}");
             Console.WriteLine($"执行耗时：{time.ToString("HH:mm:ss.fff")}");
             Console.WriteLine($"所有文件数量：{fileCount}");
             Console.WriteLine($"空文件夹个数：{emptyDirectoryCount}");
+            Console.WriteLine("");
+            #endregion
+
+            #region 同步非递归方式
+
+            //重置计时
+            stopwatch.Reset();
+
+            //开始计时
+            stopwatch.Start();
+
+            GetDirectoryCount1(root);
+
+            //停止计时
+            stopwatch.Stop();
+
+            //获取耗时
+            timeSpan = stopwatch.Elapsed;
+
+            //时间转换
+            time = ConvertToDateTime(timeSpan);
+
+            Console.WriteLine($"非递归方式");
+            Console.WriteLine($"运行框架：{RuntimeInformation.FrameworkDescription}");
+            Console.WriteLine($"操作系统：{RuntimeInformation.OSDescription}");
+            Console.WriteLine($"平台架构：{RuntimeInformation.OSArchitecture}");
+            Console.WriteLine($"执行耗时：{time.ToString("HH:mm:ss.fff")}");
+            Console.WriteLine($"所有文件数量：{fileCount}");
+            Console.WriteLine($"空文件夹个数：{emptyDirectoryCount}");
+            Console.WriteLine("");
+            #endregion
 
             //重新开始
             GetCount();
         }
 
         /// <summary>
-        /// 获取目录下的文件数量
-        /// </summary>
-        /// <param name="pathInfo">目录信息</param>
-        static int GetFileCount(DirectoryInfo pathInfo)
-        {
-            return pathInfo.GetFiles().Length;
-        }
-
-        /// <summary>
-        /// 遍历子目录下的文件数量和空文件夹数量
+        /// 递归子目录下的文件数量和空文件夹数量
         /// </summary>
         /// <param name="pathInfo">目录信息</param>
         /// <returns></returns>
@@ -134,11 +159,75 @@ namespace GetDirectoryCount
         }
 
         /// <summary>
+        /// 非递归目录下的文件数量和空文件夹数量
+        /// </summary>
+        /// <param name="pathInfo">目录信息</param>
+        static void GetDirectoryCount1(DirectoryInfo pathInfo)
+        {
+            //清零
+            fileCount = 0;
+            emptyDirectoryCount = 0;
+
+            //文件夹栈
+            var stackFolders = new Stack<DirectoryInfo>();
+
+            //将根目录入栈
+            stackFolders.Push(pathInfo);
+
+            //默认为根目录，为了排除System Volume Information
+            bool isRoot = true;
+
+            while (stackFolders.Count > 0)
+            {
+                //出栈
+                var folder = stackFolders.Pop();
+
+                //获取目录下的文件数量
+                int fCount = folder.GetFiles().Length;
+
+                //所有文件数量递增
+                fileCount += fCount;
+
+                //获取目录下的子文件夹，如果是根目录就排除System Volume Information文件夹
+                var folders = isRoot == true 
+                    ? folder.GetDirectories().Where(s => s.Name != "System Volume Information").ToArray()
+                    : folder.GetDirectories();
+
+                if (isRoot == true)
+                    isRoot = false;
+
+                //子文件夹数量
+                int dCount = folders.Length;
+                for (int i = 0; i < dCount; i++)
+                {
+                    //将子文件夹入栈
+                    stackFolders.Push(folders[i]);
+                }
+
+                //如果文件数量和子文件夹数量都为零
+                if (fCount == 0 && dCount == 0)
+                {
+                    //空目录递增
+                    emptyDirectoryCount++;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 获取目录下的文件数量
+        /// </summary>
+        /// <param name="pathInfo">目录信息</param>
+        private static int GetFileCount(DirectoryInfo pathInfo)
+        {
+            return pathInfo.GetFiles().Length;
+        }
+
+        /// <summary>
         /// 时间戳转换为日志
         /// </summary>
         /// <param name="timeSpan">时间戳</param>
         /// <returns></returns>
-        static DateTime ConvertToDateTime(TimeSpan timeSpan)
+        private static DateTime ConvertToDateTime(TimeSpan timeSpan)
         {
             var start = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
             return start.AddMilliseconds(timeSpan.TotalMilliseconds);
@@ -148,24 +237,32 @@ namespace GetDirectoryCount
         /// 递增所有文件总数
         /// </summary>
         /// <param name="num">数量</param>
-        static void IncFileCount(int num)
+        private static void IncFileCount(int num)
         {
-            lock(lockFileCountObject)
+            /*
+            lock (lockFileCountObject)
             {
                 fileCount += num;
-            }
+            }*/
+
+            //不会导致阻塞
+            Interlocked.Add(ref fileCount, num);
         }
 
         /// <summary>
         /// 递增空目录总数
         /// </summary>
         /// <param name="num">数量</param>
-        static void IncEmptyDirectoryCount(int num)
+        private static void IncEmptyDirectoryCount(int num)
         {
+            /*
             lock (lockEmptyDirectoryCountObject)
             {
                 emptyDirectoryCount += num;
-            }
+            }*/
+
+            //不会导致阻塞
+            Interlocked.Add(ref emptyDirectoryCount, num);
         }
     }
 }
